bd9022ee12d234d1bdde3bceea423b3fcad25169
Marcos Alvarez <marcosalvare@gmail.com>
2015-09-02 09:24:22 +0200

    Fixed a problem crashing the app

--- Download Manager/DownloadManager.m
+++ Download Manager/DownloadManager.m
@@ -169,7 +169,8 @@
     // while there are downloads waiting to be started and we haven't hit the maxConcurrentDownloads, then start
     @synchronized(self.downloads)
     {
-        while ([self countUnstartedDownloads] > 0 && [self countActiveDownloads] < self.maxConcurrentDownloads && self.downloads)
+        if ([self countUnstartedDownloads] > 0 && [self countActiveDownloads] < self.maxConcurrentDownloads && self.downloads)
+            //while ([self countUnstartedDownloads] > 0 && [self countActiveDownloads] < self.maxConcurrentDownloads && self.downloads)
         {
             for (Download *download in self.downloads)
             {
